8a9aee5bbef5d6ecf95fdb8681e38463dc6c1f61
Ivan Gonzalez <Ivanglez43@gmail.com>
2016-10-04 17:53:35 -0700

    AccountViewSet created, will be used for registration

--- bike_share/bike_share/urls.py
+++ bike_share/bike_share/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework_nested import routers
+from authentication.views import AccountViewSet
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/v1/', include(router.urls)),
 ]
