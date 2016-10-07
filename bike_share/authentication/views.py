73fa53b5f32195bf119b0c5af7e7723e3ef76a7e
Ivan Gonzalez <Ivanglez43@gmail.com>
2016-10-07 12:15:16 -0700

    LoginView implemented

--- bike_share/authentication/views.py
+++ bike_share/authentication/views.py
@@ -1,9 +1,43 @@
+import json
+
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
+
+from rest_framework import status, views
 from rest_framework import permissions, viewsets
+from rest_framework.response import Response
 
 from authentication.models import Account
 from authentication.permissions import IsAccountOwner
 from authentication.serializers import AccountSerializer
+
+class LoginView(views.APIView):
+    def post(self, request, format=None):
+        data = json.loads(request.body)
+
+        email = data.get('email', None)
+        password = data.get('password', None)
+
+        account = authenticate(email=email, password=password)
+
+        if account is not None:
+            if account.is_active:
+                login(request, account)
+
+                serialized = AccountSerializer(account)
+
+                return Response(serialized.data)
+            else:
+                return Response({
+                        'status': 'Unauthorized',
+                        'message': 'This account has been disabled'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                return Response({
+                        'status' : 'Unauthorized',
+                        'message' : 'username/password combination invalid'
+                }, status=status.HTTP_401_UNAUTHORIZED)
+
 
 class AccountViewSet(viewsets.ModelViewSet):
     lookuo_field = 'username'
